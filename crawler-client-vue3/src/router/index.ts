17c6aa913fbe2949ec52abb6004a2c86cc0fc4da
slybootslion <slybootslion@vip.qq.com>
2021-02-20 18:14:43 +0800

    2021-02-20 18:14:43

--- crawler-client-vue3/src/router/index.ts
+++ crawler-client-vue3/src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
 import Home from '../views/Home.vue'
 
 const routes: Array<RouteRecordRaw> = [
@@ -30,7 +30,7 @@ const routes: Array<RouteRecordRaw> = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(process.env.BASE_URL),
   routes,
 })
 
