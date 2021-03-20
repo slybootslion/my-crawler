c4c5339e1d9bd499a261808ce9d77a770c447398
SlybootsLion <slybootslion@vip.qq.com>
2021-03-20 10:13:06 +0800

    2021-03-20 10:13:06

--- node-crawler-koa/app/dao/hook/createDriver.js
+++ node-crawler-koa/app/dao/hook/createDriver.js
@@ -11,15 +11,3 @@ export const openChrome = async url => {
   await driver.get(url)
   return driver
 }
-
-
-/*
-cat << EOF > /etc/yum.repos.d/google-chrome.repo
-[google-chrome]
-name=google-chrome
-baseurl=http://dl.google.com/linux/chrome/rpm/stable/x86_64
-enabled=1
-gpgcheck=1
-gpgkey=https://dl.google.com/linux/linux_signing_key.pub
-EOF
-* */
