701dbc36dd1e3891d4d40efd60fe3d5d48c58303
SlybootsLion <slybootslion@vip.qq.com>
2021-03-20 09:54:56 +0800

    2021-03-20 09:54:56

--- node-crawler-koa/app/dao/hook/createDriver.js
+++ node-crawler-koa/app/dao/hook/createDriver.js
@@ -1,7 +1,9 @@
 import { Builder } from 'selenium-webdriver'
+import { Options } from 'selenium-webdriver/chrome'
 
 export const creatDriver = async () => {
-  return new Builder().forBrowser('chrome').build()
+  const opts = new Options()
+  return new Builder().setChromeOptions(opts.headless()).forBrowser('chrome').build()
 }
 
 export const openChrome = async url => {
@@ -9,3 +11,15 @@ export const openChrome = async url => {
   await driver.get(url)
   return driver
 }
+
+
+/*
+cat << EOF > /etc/yum.repos.d/google-chrome.repo
+[google-chrome]
+name=google-chrome
+baseurl=http://dl.google.com/linux/chrome/rpm/stable/x86_64
+enabled=1
+gpgcheck=1
+gpgkey=https://dl.google.com/linux/linux_signing_key.pub
+EOF
+* */
