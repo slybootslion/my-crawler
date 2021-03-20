1a318d082dc03904b19bc669c8c9b8d0f38b399f
SlybootsLion <slybootslion@vip.qq.com>
2021-03-20 10:32:11 +0800

    2021-03-20 10:32:11

--- node-crawler-koa/app/config/setting.js
+++ node-crawler-koa/app/config/setting.js
@@ -2,8 +2,6 @@ const path = require('path')
 
 const NODE_ENV = process.env.NODE_ENV
 const isProduction = NODE_ENV && NODE_ENV.trim() === 'production'
-const uploadBasePath = isProduction ? '/www/wwwroot/static' : 'D:\\Code\\web-example\\static'
-const uploadBaseURL = isProduction ? 'https://static.slybootslion.com/' : 'http://static1.sketchmac.com/'
 
 module.exports = {
   port: 39004,
@@ -16,8 +14,6 @@ module.exports = {
   staticPath: path.resolve(__dirname, '../../dist'),
   accessExp: 60 * 60 + 30, // 2h + 0.5分钟 单位秒
   refreshExp: 60 * 60 * 24 * 7, // 设置refresh_token的过期时间，默认7天
-  uploadBasePath,
-  uploadBaseURL,
   maxFileSize: 10 * 1024 * 1024, // 10m
   captchaExp: 60 * 10, // 验证码过期时间 (10分钟)
 }
