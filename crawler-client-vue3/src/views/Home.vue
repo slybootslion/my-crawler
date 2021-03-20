795d56fb426202c3cd400a862e10040c2936bde6
SlybootsLion <slybootslion@vip.qq.com>
2021-03-20 12:35:42 +0800

    update

--- crawler-client-vue3/src/views/Home.vue
+++ crawler-client-vue3/src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <h1 class="title">一个帮助爬取页面的小工具 by SlybootsLion</h1>
     <div class="txt">快速将掘金、知乎、微信公众号内容转为markdown</div>
-    <div class="txt">批量下载半次元高清无水印图片。</div>
+    <div class="txt">批量下载bcy高清无水印图片。</div>
     <div class="block"></div>
     <h1 class="title">使用必读：</h1>
     <p class="txt fs16">可能用的人也是为了下bcy的图，还是要强调一下，<strong class="st">不要滥用！不要滥用！不要滥用！</strong> 如果bcy更改了大图地址，大家都用不了。</p>
