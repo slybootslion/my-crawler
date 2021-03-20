874608920aa462d2bf8708bb6675dcc28a12c14f
SlybootsLion <slybootslion@vip.qq.com>
2021-03-20 12:22:29 +0800

    update Home page

--- crawler-client-vue3/src/views/Home.vue
+++ crawler-client-vue3/src/views/Home.vue
@@ -3,11 +3,11 @@
     <h1 class="title">一个帮助爬取页面的小工具 by SlybootsLion</h1>
     <div class="txt">快速将掘金、知乎、微信公众号内容转为markdown</div>
     <div class="txt">批量下载半次元高清无水印图片。</div>
+    <div class="block"></div>
+    <h1 class="title">使用必读：</h1>
+    <p class="txt fs16">可能用的人也是为了下bcy的图，还是要强调一下，<strong class="st">不要滥用！不要滥用！不要滥用！</strong> 如果bcy更改了大图地址，大家都用不了。</p>
 
-    <div>
-      使用必读：
-      <p></p>
-    </div>
+    <strong class="st txt">吐槽：早知道要布到线上，我就应该用python写。</strong>
   </div>
 </template>
 
@@ -23,11 +23,23 @@ export default defineComponent({
 .title {
   line-height: 2.6;
   font-weight: 700;
-  font-size: 18px;
+  font-size: 22px;
 }
 
 .txt {
   font-size: 12px;
   line-height: 2;
+
+  &.fs16 {
+    font-size: 18px;
+  }
+}
+
+.block{
+  height: 60px;
+}
+
+.st {
+  font-weight: 700;
 }
 </style>
